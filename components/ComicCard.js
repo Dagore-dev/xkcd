26c67a446f61dff36a04639954983a0e52012f4a
Dagore <david.gomez.redondo.dev@gmail.com>
2022-08-24 16:43:18 +0200

    gitignore updated

--- components/ComicCard.js
+++ components/ComicCard.js
@@ -1,14 +1,14 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-export default function ComicCard({ content: { id, title, img, alt } }) {
+export default function ComicCard({ content: { id, title, img, alt, width, height } }) {
   return (
     <section className='p-3'>
       <h2 className='text-3xl text-center pb-2'>{title}</h2>
       <div className='w-[300px] mx-auto'>
         <Link href={`/comic/${id}`}>
           <a>
-            <Image src={img} alt={alt} width={300} height={300} objectFit='contain' />
+            <Image src={img} alt={alt} width={width} height={height} layout='responsive' />
           </a>
         </Link>
       </div>
