1e83779a16bcfac829cbcf791936c904ac376c8c
Dagore <david.gomez.redondo.dev@gmail.com>
2022-09-05 19:53:46 +0200

    input search work but not as SPA

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -2,8 +2,15 @@ import Head from 'next/head'
 import PrimaryLayout from './PrimaryLayout'
 import PrimaryContainer from './PrimaryContainer'
 import H1 from './H1'
+import { useState } from 'react'
 
 export default function SearchForm() {
+  const [searchValue, setSearchValue] = useState('')
+
+  function handleChange(e) {
+    setSearchValue(e.target.value)
+  }
+
   return (
     <>
       <Head>
@@ -16,9 +23,15 @@ export default function SearchForm() {
           <H1>Find your favorites xkcd comics</H1>
 
           <form>
-            <input type='search' />
-            <button>Search</button>
+            <input type='search' name='q' onChange={handleChange} value={searchValue} className='border-2 border-indigo-600 px-1 rounded' />
+            <button className='bg-indigo-600 w-20 rounded-lg text-center py-1 mx-2'>
+              <span className='text-white opacity-70 hover:opacity-100 duration-300'>
+                Search
+              </span>
+            </button>
           </form>
+
+          <H1>{searchValue}</H1>
         </PrimaryContainer>
       </PrimaryLayout>
     </>
