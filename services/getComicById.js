c256fdab4a376f0efd65dbc2296f543b1fe78368
Dagore <david.gomez.redondo.dev@gmail.com>
2022-09-11 17:44:08 +0200

    bug fix in comicPath creation

--- services/getComicById.js
+++ services/getComicById.js
@@ -1,13 +1,14 @@
 import fs from 'fs-extra'
+import path from 'node:path'
 
 export default async function getComicById(id) {
-  const path = `comics/${id}.json`
-  const fileExists = await fs.pathExists(path)
+  const comicPath = path.join(process.cwd(), `comics/${id}.json`)
+  const fileExists = await fs.pathExists(comicPath)
 
   if (!fileExists) return { status: 404, content: { message: `Comic with id: "${id}" not found` } }
 
   try {
-    const comic = await fs.readJSON(path)
+    const comic = await fs.readJSON(comicPath)
     return { status: 200, content: comic }
   } catch (error) {
     return { status: 500, content: { message: 'Internal server error' } }
